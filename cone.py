8d6b7143f6b18d2d0fcaaec5762cef14d50f79eb
Francis Clairicia-Rose-Claire-Josephine <fclairicia-rcj@linux.home>
2019-12-31 15:08:19 +0100

    Added text formatting

--- cone.py
+++ cone.py
@@ -10,4 +10,7 @@ import math
 from point import Point, Vector
 
 def intersection_with_cone(point, vector, angle):
+    print("Cone with a {} degree angle".format(angle))
+    print("Line passing through the point ({}, {}, {})".format(point.x, point.y, point.z), end=" ")
+    print("and parallel to the vector ({}, {}, {})".format(vector.x, vector.y, vector.z))
     return None
--- cylinder.py
+++ cylinder.py
@@ -10,4 +10,7 @@ import math
 from point import Point, Vector
 
 def intersection_with_cylinder(point, vector, radius):
+    print("Cylinder of radius {}".format(radius))
+    print("Line passing through the point ({}, {}, {})".format(point.x, point.y, point.z), end=" ")
+    print("and parallel to the vector ({}, {}, {})".format(vector.x, vector.y, vector.z))
     return None
